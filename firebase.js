c99c62b5dd8d329675f93c30c8d35d1598862ebe
madhavan-c00 <cmadhavan521@gmail.com>
2026-04-26 23:14:10 +0530

    fix illegal characters in firebase config

--- firebase.js
+++ firebase.js
@@ -5,13 +5,13 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+  apiKey: process.env.FIREBASE_API_KEY?.trim(),
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN?.trim(),
+  projectId: process.env.FIREBASE_PROJECT_ID?.trim(),
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET?.trim(),
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID?.trim(),
+  appId: process.env.FIREBASE_APP_ID?.trim(),
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID?.trim()
 };
 
 // Initialize Firebase
